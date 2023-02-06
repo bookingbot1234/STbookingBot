aea3046f175a410a77f3a9ed41cd595cb32bf0be
bookingbot1234 <124533171+bookingbot1234@users.noreply.github.com>
2023-02-06 12:06:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@
 BOT_TOKEN = "6035984815:AAH1a_4m5kMb-IQeG6egwXIEf5WPSpSi-Rg"
 BASEROW_TOKEN = "qXPH2uMK7d0VLLg3SNHvT6ZCtJFwVw8m"
 BASEROW_EMAIL = "1tpt.jjbot@gmail.com"
+WEBHOOK = True
+WEBHOOK_PATH = env.str("WEBHOOK_PATH", default = "webhook")
+WEBHOOK_URL = f'https://stbookingbot.onrender.com/{WEBHOOK_PATH}'
