24cb7e3129ba5b581968a6efa3c82973ea5a0e83
bookingbot1234 <124533171+bookingbot1234@users.noreply.github.com>
2023-02-06 12:28:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         )
         runner.run_app(
             host="0.0.0.0",
-            port=8080
+            port=5000
         )
     else:
         logger.info("Running in Polling Mode")
