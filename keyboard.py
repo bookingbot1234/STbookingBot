168253517e683c78ebe3c9e052cc3699d3f537ea
bookingbot1234 <124533171+bookingbot1234@users.noreply.github.com>
2023-02-08 18:21:05 +0800

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -24,8 +24,24 @@ def rank_keyboard():
     markup.add("MSG")
     return markup
 
-def coy_keyboard():
+
+def hub_keyboard():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("HQ TPT")
+    markup.add("1 TPT")
+    markup.add("3 TPT")
+    markup.add("WEST")
+    markup.add("EAST")
+    return markup
+
+def HQTPT_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("HQ TPT")
+    return markup
+
+def first_coy_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("1TPT HQ")
     markup.add("ALPHA")
     markup.add("CHARLIE")
     markup.add("HMCT")
@@ -34,6 +50,37 @@ def coy_keyboard():
     markup.add("KRANJI")
     markup.add("LTC")
     return markup
+
+def third_coy_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("3TPT HQ")
+    markup.add("AIR TERMINAL")
+    markup.add("SEA TERMINAL")
+    markup.add("LARC V")
+    markup.add("CHANGI")
+    markup.add("TUAS")
+    markup.add("CLEMENTI")
+    return markup
+
+def east_coy_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("EAST HQ")
+    markup.add("SELARANG")
+    markup.add("SELETAR")
+    markup.add("NEE SOON")
+    markup.add("BEDOK")
+    markup.add("TEKONG")
+    return markup
+
+def west_coy_keyboard():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    markup.add("WEST HQ")
+    markup.add("JURONG")
+    markup.add("KEAT HONG")
+    markup.add("PASIR LABA")
+    markup.add("SUNGEI GEDONG")
+    return markup
+
 
 def confirmation_keyboard():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
